b8d4fee4ea6d0ba6804191e74a02441da4813fee
Valentine <abraval@gmail.com>
2019-10-23 14:46:52 -0400

    had to go back to a previous version, reoganize the code, add renserQuestion function and win condition

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
-
+var correct = 0;
+var wrong = 0;
+var questionIndex = 0;
 
 $("#start-btn").click(function () {
     $("#intro").hide();
@@ -13,6 +15,7 @@ $(".answ-btn").click(function () {
 $("#cont-btn").click(function () {
     $("#y_n").hide();
     $("#q_a").css("display", "block")
+    questionIndex++;
 });
 
 var questions = [
@@ -25,7 +28,7 @@ var questions = [
         correct: "7"
     },
     {
-        q: "What is the name of Jasmine’s tiger?",
+        q: "What is the name of Jasmine's tiger?",
         a1: "Raja",
         a2: "Apu",
         a3: "Sher",
@@ -49,7 +52,7 @@ var questions = [
         correct: "An apple"
     },
     {
-        q: "Who is Rapunzel’s love interest?",
+        q: "Who is Rapunzel's love interest?",
         a1: "Prince Eric",
         a2: "Prince Adam",
         a3: "John Smith",
@@ -81,7 +84,7 @@ var questions = [
         correct: "Agrabah"
     },
     {
-        q: "Who is Sleeping Beauty’s true love?",
+        q: "Who is Sleeping Beauty's true love?",
         a1: "Prince Charming",
         a2: "Prince Phillip",
         a3: "Prince Adam",
@@ -89,7 +92,7 @@ var questions = [
         correct: "Prince Phillip"
     },
     {
-        q: "Who is Ariel’s best friend?",
+        q: "Who is Ariel's best friend?",
         a1: "Ursula",
         a2: "Dory",
         a3: "Flounder",
@@ -101,6 +104,7 @@ var questions = [
 
 $("#start-btn").on("click", start);
 $("#cont-btn").on("click", start);
+$(".answ-btn").on("click", stop);
 
 var intervalId;
 var clockRunning = false;
@@ -109,19 +113,19 @@ var time = 20;
 function reset() {
     time = 20;
     $("#timer").text("00:00");
-  }
+}
 
-  function start() {
+function start() {
     time = 20;
     $("#timer").text("00:20");
     console.log(time)
     if (!clockRunning) {
-      intervalId = setInterval(count, 1000);
-      clockRunning = true;
+        intervalId = setInterval(count, 1000);
+        clockRunning = true;
     }
-  }
+}
 
-  function count() {
+function count() {
     time--;
     var converted = timeConverter(time);
     console.log(converted);
@@ -130,24 +134,71 @@ function reset() {
         $("#q_a").hide();
         $("#noTime").css("display", "block")
     }
-  }
-  function timeConverter(t) {
-  
+}
+function stop() {
+    clearInterval(intervalId);
+    clockRunning = false;
+    var converted = timeConverter(time);
+    console.log(converted)
+    $("#stopTime").text(converted);
+}
+function timeConverter(t) {
+
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
-  
+
     if (seconds < 10) {
-      seconds = "0" + seconds;
+        seconds = "0" + seconds;
     }
-  
+
     if (minutes === 0) {
-      minutes = "00";
+        minutes = "00";
     }
     else if (minutes < 10) {
-      minutes = "0" + minutes;
+        minutes = "0" + minutes;
     }
-  
-    return minutes + ":" + seconds;
-  }
-  
 
+    return minutes + ":" + seconds;
+}
+function renderQuestion() {
+console.log("Render function")
+
+console.log(questionIndex);
+console.log(questions.length)
+    if (questionIndex <= (questions.length - 1)) {
+        document.querySelector("#question").innerHTML = questions[questionIndex].q;
+        document.querySelector("#answer1").innerHTML = questions[questionIndex].a1;
+        document.querySelector("#answer2").innerHTML = questions[questionIndex].a2;
+        document.querySelector("#answer3").innerHTML = questions[questionIndex].a3;
+        document.querySelector("#answer4").innerHTML = questions[questionIndex].a4;
+    }else if (questionIndex === questions.length) {
+        $("#y_n").hide();
+        $("#q_a").hide();
+        $("#final").css("display", "block")
+        document.querySelector("#corTally").innerHTML = correct;
+        document.querySelector("#wrongTally").innerHTML = wrong;
+        return;
+        // $("#corTally").text(correct);
+        // $("#wrongTally").text(wrong);
+    }
+}
+$(".qbtn").on("click", renderQuestion);
+$(".answ-btn").on("click", function () {
+
+    var userChoice = $(this).text();
+    console.log(userChoice);
+    if (userChoice === questions[questionIndex].correct) {
+        correct++;
+        $("#yesOrNo").text("You you correct!");
+        $("#itWas").text(questions[questionIndex].correct);
+        displayImage();
+
+    }
+    else {
+        wrong++;
+        $("#yesOrNo").text("Oh no! It's wrong!");
+        $("#itWas").text(questions[questionIndex].correct);
+        displayImage();
+
+
+    }})
