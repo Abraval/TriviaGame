efa6f121fae3afc66ec47797e476184558af86ac
Valentine <abraval@gmail.com>
2019-10-22 22:01:57 -0400

    minor formating corrections in index.html and app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,21 +3,15 @@
 $("#start-btn").click(function(){
     $("#intro").hide();
     $("#q_a").css("display", "block")
-
-
   });
 
   $(".answ-btn").click(function(){
     $("#q_a").hide();
     $("#y_n").css("display", "block")
-
-
   });
 
   $("#cont-btn").click(function(){
     $("#y_n").hide();
     $("#q_a").css("display", "block")
-
-
   });
 
